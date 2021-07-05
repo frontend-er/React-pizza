67e956f9df8a89c4a9a14251d62458facf0d6bde
Дмитрий <daniil@MacBook-Pro-Daniil.local>
2021-07-05 13:47:24 +0300

    herocu

--- src/components/index.js
+++
@@ -1,5 +0,0 @@
-export { default as Button } from './Button';
-export { default as Header } from './Header';
-export { default as Categories } from './Categories';
-export { default as ShortPopup } from './ShortPopup';
-export { default as PizzaBlock } from './PizzaBlock';
--- src/redux/action/pizzas.js
+++
@@ -1,9 +0,0 @@
-
-
-export const setPizzas = (items) => ({
-   type: 'SET_PIZZAS',
-   payload: items,
-})
-
-
-
